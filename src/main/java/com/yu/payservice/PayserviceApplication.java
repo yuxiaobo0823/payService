658517639a78bc5386903501ec150d7d2f89bd2d
yuxiaobo0823 <944836881@qq.com>
2022-04-16 18:18:25 +0800

    payservice

---
+++ src/main/java/com/yu/payservice/PayserviceApplication.java
@@ -0,0 +1,13 @@
+package com.yu.payservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yu/payservice/PayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.payservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
